cacf6388b59430b889bd7bad7c5f4729ac53c573
c0d3runn3r <joseph.sullivan@active911.com>
2016-12-05 01:47:08 -0800

    Displays stats on exit

--- analyze.js
+++ analyze.js
@@ -3,6 +3,8 @@ var promise=require("bluebird");
 var fs=promise.promisifyAll(require("fs"));
 var linebyline=require("line-by-line");
 var nconf = require('nconf');
+var crossfilter = require('crossfilter');
+
 
 // Read the config
 nconf.argv().env().file({ file: 'config.json' });
@@ -20,6 +22,23 @@ var al=new asterisklog({queues: nconf.get('asterisk').queues});
 			.then(()=>{
 
 				console.log(calls.length+" calls written");
+
+			})
+			.then(()=>{
+
+				// Analyze with crossfilter
+				var cf=crossfilter(calls);
+				var extension_dim=cf.dimension((d)=>d.answered_by);
+				var extension_group=extension_dim.group();
+				console.log("Here is where all the calls went:");
+				console.log(JSON.stringify(extension_group.all()));
+
+				// Dimension hold time, and get only calls that did hold
+				var queue_time_dim=cf.dimension((d)=>(d.enqueued && d.answered)?(d.answered-d.enqueued)/1000:0);
+				queue_time_dim.filter((v)=>(v!=0));
+				var queuers=queue_time_dim.top(Infinity);
+				console.log("Average hold time: "+queuers.reduce((p,v)=>{return p+((v.answered-v.enqueued)/1000);},0)/queuers.length);
+
 
 			})
 			.catch((err)=>{
