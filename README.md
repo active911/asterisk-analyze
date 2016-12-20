2d4fc17a22d6356648f4abe48fa12b8f56453cdf
c0d3runn3r <joseph.sullivan@active911.com>
2016-12-20 04:03:02 -0800

    etl working with MySQL

--- README.md
+++ README.md
@@ -13,15 +13,27 @@ cp config.json.sample config.json
 vi config.cfg
 ```
 
+#### SQL
+```sql
+CREATE TABLE calls (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+start datetime not null,
+data text null
+)ENGINE=innodb;
+```
+
 #### Run the program
-1. ```node analyze.js``` to generate calls.json
+1. ``` node etl.js``` to run the ETL daemon (add ```--gulp``` to ingest the whole file)
 2. ```npm run build``` to build
 3. ```npm start``` to start the server
 4. Navigate to server/index.html to see the pretty graphs
 
 
-#### TODO
+In ```--gulp``` mode, etl will read the whole input file specified in the config and exit rather than following file modificaitons.  It will also merge the new data with the unclosed calls in the database.  This is slower, but keeps you from adding duplicates.
 
+Without the ```--gulp``` switch (or gulp in the config), etl watches the input file for new data.
+
+#### TODO
 - calls.json should not be embedded in the build (go to AJAX)
 - Use bootstrap to organize the views
 - Add more graphs
---
+++ etl.js
@@ -0,0 +1,113 @@
+var promise=require("bluebird");
+var asterisklog=require("./lib/asterisklog.js");
+var nconf = require('nconf');
+var fs=promise.promisifyAll(require("fs"));
+var linebyline=require("line-by-line");
+var Tail = require('always-tail2');
+var log = require("bunyan").createLogger({"name":"asterisk-analyze-etl"});
+var mysql = require("promise-mysql");
+var moment = require("moment");
+
+
+// Read the config
+nconf
+	.argv()
+	.env()
+	.file({ file: 'config.json' })
+	.defaults({ "general" : { "input" : "full", "output" : "src/calls.json", "mode" : "follow" } });
+
+// Create analyzer
+var al=new asterisklog({queues: nconf.get('asterisk').queues});
+
+// Ensure input file exists
+if(!fs.existsSync(nconf.get('general').input)){
+
+	log.error("Error - cannot read from input file '"+nconf.get('general').input+"'");
+}
+
+
+// Connect to the databsae
+mysql
+	.createConnection(nconf.get('mysql'))
+	.then((c)=>{
+
+		log.info("Connected to database");
+
+		// A new call has been found
+		al.on("start", (call) => {
+
+			log.info("New call started");
+		})
+		.on("end", (call) => {
+
+			log.info("Call ended");
+
+
+			// See if this record already exists
+			c
+				.query("SELECT id, data FROM calls WHERE start = ?", [moment(call.start).format('YYYY-MM-DD HH:mm:ss')])
+				.then((rows) =>{
+
+
+					for(n in rows) {
+
+						var data=JSON.parse(rows[n].data);
+						if (call.id == data.id){
+
+							if(call.end) {
+
+								// This is a match.  Call already listed as ended.  Do not insert.
+								log.info("Existing call found.  Not saving in database");
+								return promise.resolve(false);
+							}
+						}
+					}
+
+					// No matching call.  Insert
+					log.info("Inserting new call into database");
+					return promise.resolve(true);
+
+				})
+				.then((b)=>{
+
+					if(b) c.query("INSERT INTO calls (start, data) VALUES ( ?, ?)",[moment(call.start).format('YYYY-MM-DD HH:mm:ss'),JSON.stringify(call)]);
+				});		
+		});	
+
+		// Start watching
+		if(reader instanceof Tail) reader.watch();
+		return promise.resolve(true);
+	})
+	.catch((err) =>{
+
+		log.error(err);
+		if(reader instanceof Tail) reader.unwatch();
+		log.error("Quitting");
+		exit();
+	});
+
+
+// Select a reader
+var reader = (nconf.get('gulp'))?(new linebyline(nconf.get('general').input)):(new Tail(nconf.get('general').input));
+
+reader
+	.on("line", (line)=>al.add(line))
+	.on('error', (err) => {
+
+		log.error("Error - "+err);
+		log.error("Quitting");
+		if(reader instanceof Tail) reader.unwatch();
+		exit();
+	})
+	.on("end",()=>{
+
+		if(reader instanceof linebyline) {
+			
+			process.exit();
+		};
+		exit();		
+	});
+
+
+
+
